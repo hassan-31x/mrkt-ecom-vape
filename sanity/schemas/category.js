6b0b65bb35d54665af3338b9e73841fb9bc0d7b6
Hassan <muhammadhassanchannel786@gmail.com>
2024-01-15 21:37:14 +0500

    feat: added sanity

---
+++ sanity/schemas/category.js
@@ -0,0 +1,17 @@
+export const category = {
+  name: "category",
+  title: "Blog - Category",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "description",
+      title: "Description",
+      type: "text",
+    },
+  ],
+};

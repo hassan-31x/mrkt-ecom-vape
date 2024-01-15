a20d3b36fce57960b76c6521b7c5d9a14c79ac1f
Hassan <muhammadhassanchannel786@gmail.com>
2024-01-15 18:04:56 +0500

    fix: fixed react-toastify server-side error

--- components/custom-layout.jsx
+++ components/custom-layout.jsx
@@ -1,11 +1,12 @@
-'use client'
+"use client";
 
 import { useEffect } from "react";
-import { ToastContainer } from 'react-toastify';
-import { useRouter } from 'next/navigation';
+import { ToastContainer } from "react-toastify";
+import { useRouter } from "next/navigation";
 
 // import 'react-image-lightbox/style.css';
-import 'react-toastify/dist/ReactToastify.min.css';
+// import 'react-toastify/dist/ReactToastify.min.css';
+import "react-toastify/dist/ReactToastify.css";
 
 import Header from "./partials/header/header";
 import Footer from "./partials/footer/footer";
@@ -15,85 +16,85 @@ import QuickViewModal from "./features/modals/quickview-modal";
 
 import { isSafariBrowser, isEdgeBrowser } from "@/utils";
 
-function Layout ( { children, hideQuick, hideVideo } ) {
-    const router = useRouter( "" );
-    let scrollTop;
+function Layout({ children, hideQuick, hideVideo }) {
+  const router = useRouter("");
+  let scrollTop;
 
-    useEffect( () => {
-        if ( router?.pathname?.includes( 'pages/coming-soon' ) ) {
-            document.querySelector( "header" ).classList.add( "d-none" );
-            document.querySelector( "footer" ).classList.add( "d-none" );
-        } else {
-            document.querySelector( "header" ).classList.remove( "d-none" );
-            document.querySelector( "footer" ).classList.remove( "d-none" );
-        }
-    }, [ router?.pathname ] )
-
-    useEffect( () => {
-        // hideQuick();
-        // hideVideo();
-        scrollTop = document.querySelector( '#scroll-top' );
-        window.addEventListener( 'scroll', scrollHandler, false );
-    }, [] )
-
-    function toScrollTop () {
-        if ( isSafariBrowser() || isEdgeBrowser() ) {
-            let pos = window.pageYOffset;
-            let timerId = setInterval( () => {
-                if ( pos <= 0 ) clearInterval( timerId );
-                window.scrollBy( 0, -120 );
-                pos -= 120;
-            }, 1 );
-        } else {
-            window.scrollTo( {
-                top: 0,
-                behavior: 'smooth'
-            } );
-        }
+  useEffect(() => {
+    if (router?.pathname?.includes("pages/coming-soon")) {
+      document.querySelector("header").classList.add("d-none");
+      document.querySelector("footer").classList.add("d-none");
+    } else {
+      document.querySelector("header").classList.remove("d-none");
+      document.querySelector("footer").classList.remove("d-none");
     }
+  }, [router?.pathname]);
 
-    function scrollHandler () {
-        if ( window.pageYOffset >= 400 ) {
-            scrollTop.classList.add( 'show' );
-        } else {
-            scrollTop.classList.remove( 'show' );
-        }
+  useEffect(() => {
+    // hideQuick();
+    // hideVideo();
+    scrollTop = document.querySelector("#scroll-top");
+    window.addEventListener("scroll", scrollHandler, false);
+  }, []);
+
+  function toScrollTop() {
+    if (isSafariBrowser() || isEdgeBrowser()) {
+      let pos = window.pageYOffset;
+      let timerId = setInterval(() => {
+        if (pos <= 0) clearInterval(timerId);
+        window.scrollBy(0, -120);
+        pos -= 120;
+      }, 1);
+    } else {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
     }
+  }
 
-    function hideMobileMenu () {
-        document.querySelector( 'body' ).classList.remove( 'mmenu-active' );
+  function scrollHandler() {
+    if (window.pageYOffset >= 400) {
+      scrollTop.classList.add("show");
+    } else {
+      scrollTop.classList.remove("show");
     }
+  }
 
-    return (
-        <>
-            <div className="page-wrapper">
-                <Header />
-                { children }
-                <Footer />
-            </div>
-            <div className="mobile-menu-overlay" onClick={ hideMobileMenu }></div>
-            <button id="scroll-top" title="Back to top" onClick={ toScrollTop }>
-                <i className="icon-arrow-up"></i>
-            </button>
-            {/* <MobileMenu /> */}
+  function hideMobileMenu() {
+    document.querySelector("body").classList.remove("mmenu-active");
+  }
 
-            <ToastContainer
-                autoClose={ 3000 }
-                duration={ 300 }
-                newestOnTo={ true }
-                className="toast-container"
-                position="top-right"
-                closeButton={ false }
-                hideProgressBar={ true }
-                newestOnTop={ true }
-                draggable={ false }
-            />
+  return (
+    <>
+      <div className="page-wrapper">
+        <Header />
+        {children}
+        <Footer />
+      </div>
+      <div className="mobile-menu-overlay" onClick={hideMobileMenu}></div>
+      <button id="scroll-top" title="Back to top" onClick={toScrollTop}>
+        <i className="icon-arrow-up"></i>
+      </button>
+      {/* <MobileMenu /> */}
 
-            <QuickViewModal />
+      <ToastContainer
+        autoClose={3000}
+        duration={300}
+        newestOnTo={true}
+        className="toast-container"
+        position="top-right"
+        closeButton={false}
+        hideProgressBar={true}
+        newestOnTop={true}
+        draggable={false}
+      />
 
-            {/* <VideoModal /> */}
-        </>
-    )
+      <QuickViewModal />
+
+      {/* <VideoModal /> */}
+    </>
+  );
 }
 
 export default Layout;

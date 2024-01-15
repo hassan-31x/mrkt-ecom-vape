953eabf3e8b466582bde0b65321c03afc5b256ae
Hassan <muhammadhassanchannel786@gmail.com>
2024-01-15 15:09:38 +0500

    feat: not found page

---
+++ app/not-found.jsx
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import React from "react";
+
+function ErrorPage() {
+  return (
+    <div className="main">
+      <nav className="breadcrumb-nav border-0 mb-0">
+        <div className="container">
+          <ol className="breadcrumb">
+            <li className="breadcrumb-item">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="breadcrumb-item">
+              <Link href="#">Pages</Link>
+            </li>
+            <li className="breadcrumb-item active">404</li>
+          </ol>
+        </div>
+      </nav>
+
+      <div
+        className="error-content text-center position-relative"
+        style={{
+          backgroundImage: `url(images/backgrounds/error-bg.jpg)`,
+          marginBottom: "-1px",
+        }}
+      >
+        <div className="container">
+          <h1 className="error-title">Error 404</h1>
+
+          <p>We are sorry, the page you've requested is not available.</p>
+          <Link href="/" className="btn btn-outline-primary-2 btn-minwidth-lg">
+            <span>BACK TO HOMEPAGE</span>
+            <i className="icon-long-arrow-right"></i>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ErrorPage;

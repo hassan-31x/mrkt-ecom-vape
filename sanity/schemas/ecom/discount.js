f9278981de36dbf7447a018af7339b5d89f7146d
Hassan <muhammadhassanchannel786@gmail.com>
2024-02-10 18:14:07 +0500

    feat: discount code schema simplified

--- sanity/schemas/ecom/discount.js
+++ sanity/schemas/ecom/discount.js
@@ -12,17 +12,16 @@ export const discount = {
       {
         name: "code",
         title: "Discount Code",
-        description: "Any code of your choice",
+        description: "Any code of your choice (optional)",
         type: "string",
-        validation: (Rule) => Rule.required(),
       },
-      {
-        name: "amount",
-        title: "Discount Amount",
-        description:
-          "enter any 1 from amount & percentage. Leave blank if not applicable",
-        type: "number",
-      },
+      // {
+      //   name: "amount",
+      //   title: "Discount Amount",
+      //   description:
+      //     "enter any 1 from amount & percentage. Leave blank if not applicable",
+      //   type: "number",
+      // },
       {
         name: "percentage",
         title: "Discount Percentage",
@@ -30,16 +29,27 @@ export const discount = {
           "enter any 1 from amount & percentage. Leave blank if not applicable",
         type: "number",
       },
-      {
-        name: "products",
-        title: "Products Included",
-        type: "array",
-        of: [
-          {
-            type: "reference",
-            to: [{ type: "product" }],
-          },
-        ],
-      },
+      // {
+      //   name: "products",
+      //   title: "Products Included",
+      //   type: "array",
+      //   of: [
+      //     {
+      //       type: "reference",
+      //       to: [{ type: "product" }],
+      //     },
+      //   ],
+      // },
     ],
+    preview: {
+      select: {
+        title: "name",
+        perc: "percentage",
+        // media: "mainImage",
+      },
+      prepare(selection) {
+        const { perc } = selection;
+        return { ...selection, subtitle: `${perc}%` };
+      },
+    },
   };

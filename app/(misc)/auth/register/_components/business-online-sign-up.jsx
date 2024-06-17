76703c67fa65dfeb805db2ae601c2a4584f9552c
Hassan <muhammadhassanchannel786@gmail.com>
2024-06-17 06:10:04 +0500

    feat: individual form register ui

---
+++ app/(misc)/auth/register/_components/business-online-sign-up.jsx
@@ -0,0 +1,268 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { toast } from "react-toastify";
+import { signIn } from "next-auth/react";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+  agreementChecked: false,
+};
+
+const BusinessOnlineSignUpComponent = ({ type }) => {
+  const [formData, setFormData] = useState(initialState);
+  const [formErrors, setFormErrors] = useState(initialState);
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+
+    setFormErrors({});
+  };
+
+  const handleCheckboxChange = (e) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      agreementChecked: e.target.checked,
+    }));
+
+    setFormErrors({});
+  };
+
+  const validateForm = (formData) => {
+    const errors = {};
+    
+    if (!formData.name.trim()) {
+      errors.name = "Please enter your full name";
+      setFormErrors(errors);
+      return false;
+    }
+
+    if (!formData.email.trim()) {
+      errors.email = "Please enter your email address";
+      setFormErrors(errors);
+      return false;
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      errors.email = "Please enter a valid email address";
+      setFormErrors(errors);
+      return false;
+    }
+
+    if (!formData.password?.trim()) {
+      errors.password = "Please enter your password";
+      setFormErrors(errors);
+      return false;
+    }
+
+    if (!formData.confirmPassword?.trim()) {
+      errors.confirmPassword = "Please re-enter your password";
+      setFormErrors(errors);
+      return false;
+    }
+
+    if (formData.confirmPassword != formData.password) {
+      errors.confirmPassword = "Passwords do not match";
+      setFormErrors(errors);
+      return false;
+    }
+
+    if (!formData.agreementChecked) {
+      errors.agreementChecked = "Please agree to the privacy policy";
+      setFormErrors(errors);
+      return false;
+    }
+
+    return true
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!validateForm(formData)) return;
+
+    setLoading(true);
+
+    try {
+      const code = searchParams.get("code");
+      // const res1 = await signUp({
+      //   name: formData.name,
+      //   email: formData.email,
+      //   password: formData.password,
+      // });
+
+      // if (code) {
+      //   const res2 = await fetch("/api/addDiscount", {
+      //     method: "POST",
+      //     headers: {
+      //       "Content-Type": "application/json",
+      //     },
+      //     body: JSON.stringify({
+      //       email: formData.email,
+      //       code,
+      //     }),
+      //   });
+      // }
+      const res = await fetch('/api/sign-up', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          ...formData,
+          accountType: type,
+        }),
+      });
+
+      const data = await res.json();
+
+      if (data.status !== 'success') {
+        toast.error(data.message);
+        return
+      }
+      toast.success(type === 'individual' ? "User Registered! Login to continue." : "Business Registered! Please wait for approval.");
+      setFormData(initialState);
+      if (type === 'individual') {
+        router.push("/auth/login");
+      }
+
+    } catch (error) {
+      console.error("Signup error:", error);
+      toast.error("Error Registering");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <form action="#">
+        <div className="form-group">
+          <div className="form-group">
+            <label htmlFor="register-name-2">Full Name *</label>
+            <input
+              type="text"
+              className="form-control"
+              id="register-name-2"
+              value={formData.name}
+              onChange={handleChange}
+              name="name"
+            />
+            {formErrors?.name && (
+              <span className="text-red-600">*{formErrors.name}</span>
+            )}
+          </div>
+
+          <label htmlFor="register-email-2">Your email address *</label>
+          <input
+            type="email"
+            className="form-control"
+            id="register-email-2"
+            value={formData.email}
+            onChange={handleChange}
+            name="email"
+          />
+          {formErrors?.email && (
+            <span className="text-red-600">*{formErrors.email}</span>
+          )}
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="register-password-21">Password *</label>
+          <input
+            type="password"
+            className="form-control"
+            id="register-password-21"
+            value={formData.password}
+            onChange={handleChange}
+            name="password"
+          />
+          {formErrors?.password && (
+            <span className="text-red-600">*{formErrors.password}</span>
+          )}
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="register-password-22">Confirm Password *</label>
+          <input
+            type="password"
+            className="form-control"
+            id="register-password-22"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+            name="confirmPassword"
+          />
+          {formErrors?.confirmPassword && (
+            <span className="text-red-600">*{formErrors.confirmPassword}</span>
+          )}
+        </div>
+
+        <div className="form-footer flex justify-between">
+          <div className="custom-control custom-checkbox">
+            <input
+              type="checkbox"
+              className="custom-control-input"
+              id="register-policy-2"
+              name="agreementChecked"
+              checked={formData.agreementChecked}
+              onChange={handleCheckboxChange}
+            />
+            <label
+              className={`custom-control-label ${
+                formErrors?.agreementChecked ? "text-red-600" : "text-black"
+              }`}
+              htmlFor="register-policy-2"
+            >
+              I agree to the privacy policy*
+            </label>
+          </div>
+
+          <button
+            type="button"
+            onClick={handleSubmit}
+            disabled={loading}
+            className="btn btn-outline-primary-2"
+          >
+            <span>{loading ? "Loading" : "Register"}</span>
+            <i className="icon-long-arrow-right"></i>
+          </button>
+        </div>
+      </form>
+      <div className="form-choice">
+        <p className="text-center">or sign in with</p>
+        <div className="row">
+          <div className="col-sm-6">
+            <button
+              className="btn btn-login btn-g w-full"
+              onClick={() => signIn("google")}
+            >
+              <i className="icon-google"></i>
+              Login With Google
+            </button>
+          </div>
+          <div className="col-sm-6">
+            <button
+              className="btn btn-login btn-f w-full"
+              // onClick={() => signIn("facebook")}
+            >
+              <i className="icon-facebook-f"></i>
+              Login With Facebook
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default BusinessOnlineSignUpComponent;
--- app/(misc)/auth/register/_components/register-form.jsx
+++ app/(misc)/auth/register/_components/register-form.jsx
@@ -1,31 +1,56 @@
 "use client";
 
 import { Tabs, TabList, Tab, TabPanel } from "react-tabs";
-import SignUpComponent from "./sign-up";
+import IndividualSignUpComponent from "./individual-sign-up";
+import BusinessOnlineSignUpComponent from "./business-online-sign-up";
+import { useState } from "react";
 
 const RegisterForm = () => {
+  const [step, setStep] = useState(1);
+
   return (
-    <Tabs selectedTabClassName="show" defaultIndex={0}>
-      <TabList className="nav nav-pills nav-fill">
-        <Tab className="nav-item">
-          <span className="nav-link">Individual</span>
-        </Tab>
+    <div>
+      {
+        step === 0 && (
+          <div className="w-full">
+            <h3 className="">Bersiaplah berkelana rasa bersama mrkt Indonesia</h3>
+            <p>
+            Tujuan Anda menggunakan produk mrkt?
+            </p>
+            <div className="flex flex-col gap-5 mt-4">
+              <button onClick={() => setStep(1)} className="w-full h-24 cursor-pointer hover:bg-slate-400 transition-all duration-100 bg-slate-300 border-[1px] border-solid !border-black text-black font-medium text-2xl rounded-2xl">Penggunaan personal/individu</button>
+              <button onClick={() => setStep(2)} className="w-full h-24 cursor-pointer hover:bg-slate-400 transition-all duration-100 bg-slate-300 border-[1px] border-solid !border-black text-black font-medium text-2xl rounded-2xl">Tujuan bisnis/komersial</button>
+            </div>
 
-        <Tab className="nav-item">
-          <span className="nav-link">Business</span>
-        </Tab>
-      </TabList>
+          </div>
+        )
+      }
+      {step === 1 &&
+        <IndividualSignUpComponent />
+      }
+    </div>
 
-      <div className="tab-content">
-        <TabPanel style={{ paddingTop: "2rem" }}>
-          <SignUpComponent type="user" />
-        </TabPanel>
+    // <Tabs selectedTabClassName="show" defaultIndex={0}>
+    //   <TabList className="nav nav-pills nav-fill">
+    //     <Tab className="nav-item">
+    //       <span className="nav-link">Individual</span>
+    //     </Tab>
 
-        <TabPanel>
-          <SignUpComponent type="business" />
-        </TabPanel>
-      </div>
-    </Tabs>
+    //     <Tab className="nav-item">
+    //       <span className="nav-link">Business</span>
+    //     </Tab>
+    //   </TabList>
+
+    //   <div className="tab-content">
+    //     <TabPanel style={{ paddingTop: "2rem" }}>
+    //       <BusinessOnlineSignUpComponent type="user" />
+    //     </TabPanel>
+
+    //     <TabPanel>
+    //       <BusinessOnlineSignUpComponent type="business" />
+    //     </TabPanel>
+    //   </div>
+    // </Tabs>
   );
 };
 

1203c46a8e27e5369bfc7f128cea92443adab555
Hassan <muhammadhassanchannel786@gmail.com>
2024-06-26 20:56:45 +0500

    custom script added in layout file

--- app/layout.jsx
+++ app/layout.jsx
@@ -50,6 +50,34 @@ export default function RootLayout({ children }) {
         </ReduxProvider>
 
         <Script src="/js/jquery.min.js" />
+        <Script id="show-banner">
+          {`(function() {
+        window.sib = {
+            equeue: [],
+            client_key: "y6x68j09m2g6n2tlov9iucnz"
+        };
+        /* OPTIONAL: email for identify request*/
+        // window.sib.email_id = 'example@domain.com';
+        window.sendinblue = {};
+        for (var j = ['track', 'identify', 'trackLink', 'page'], i = 0; i < j.length; i++) {
+        (function(k) {
+            window.sendinblue[k] = function() {
+                var arg = Array.prototype.slice.call(arguments);
+                (window.sib[k] || function() {
+                        var t = {};
+                        t[k] = arg;
+                        window.sib.equeue.push(t);
+                    })(arg[0], arg[1], arg[2]);
+                };
+            })(j[i]);
+        }
+        var n = document.createElement("script"),
+            i = document.getElementsByTagName("script")[0];
+            n.type = "text/javascript", n.id = "sendinblue-js",
+            n.async = !0, n.src = "https://sibautomation.com/sa.js?key="+ window.sib.client_key,
+            i.parentNode.insertBefore(n, i), window.sendinblue.page();
+    })();`}
+        </Script>
       </body>
       {/* <GoogleAnalytics gaId="GTM-NZVHDSM7" /> */}
       <GoogleAnalytics gaId="G-ZK0F60HHVF" />

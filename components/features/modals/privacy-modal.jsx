966f3b6a74e1f8cfae1b1ded166fd0cf443d5438
Hassan <muhammadhassanchannel786@gmail.com>
2024-07-29 17:35:02 +0500

    style: fixed modal

--- components/features/modals/privacy-modal.jsx
+++ components/features/modals/privacy-modal.jsx
@@ -29,9 +29,7 @@ function PrivacyModal() {
   }, []);
 
   function closeModal(e) {
-    document
-      .getElementById("hide-privacy-popup-modal")
-      .classList.remove("ReactModal__Content--after-open");
+    document.getElementById("hide-privacy-popup-modal").classList.remove("ReactModal__Content--after-open");
 
     if (document.querySelector(".overlay-privacy-modal")) {
       document.querySelector(".overlay-privacy-modal").style.display = "none";
@@ -45,18 +43,17 @@ function PrivacyModal() {
         expires: 7,
       });
     }, 350);
-  }
+  };
 
   const handleConfirm = () => {
     closeModal();
     storeCookie();
-
-  }
+  };
 
   const cancelConfirm = () => {
-    closeModal()
+    closeModal();
     router.back();
-  }
+  };
 
   return (
     <Modal
@@ -69,10 +66,9 @@ function PrivacyModal() {
       overlayClassName="flex items-end pb-2 justify-center overlay-privacy-modal newsletter-modal"
       id="hide-privacy-popup-modal"
     >
-      <div className="modal-content h-[200px] md:h-[300px] w-[90vw] max-w-[90vw] overflow-hidden">
+      <div className="modal-content h-[300px] w-[90vw] max-w-[90vw] overflow-hidden">
         <div className="row justify-content-center position-relative overflow-y-scroll">
-
-          <button type="button" className="close absolute top-[7px] md:top-[5px] right-[10px] md:right-[5px] z-[99999]" onClick={closeModal}>
+          <button type="button" className="close absolute top-[7px] md:top-[5px] right-[12px] md:right-[5px] z-[99999]" onClick={closeModal}>
             <span aria-hidden="true">
               <i className="icon-close"></i>
             </span>
@@ -81,53 +77,61 @@ function PrivacyModal() {
           <div className="w-full">
             <div className="w-full flex justify-center items-center bg-white newsletter-popup-content">
               <div className="banner-content">
-                
-                <h2 className="text-center text-4xl md:text-5xl font-semibold py-4">Kami menghargai privasi Anda</h2>
+                <h2 className="text-center text-4xl md:text-5xl font-semibold py-0 md:py-4">Kami menghargai privasi Anda</h2>
 
-                <div>
-                  <p>Website ini menggunakan cookie dan teknologi pelacak lainnya untuk meningkatkan pengalaman penelusuran agar lebih sesuai dengan kebutuhan Anda. Dengan mengklik “Terima”, Anda menyetujui pemrosesan data pribadi Anda sesuai dengan <Link href='/kebijakan-privasi'>Kebijakan Privasi dan Kebijakan Cookie</Link></p>
-                  <p>Kami tidak membagikan informasi pribadi Anda dengan pihak ketiga, kecuali jika dibutuhkan untuk kebutuhan profesional dan bisnis kami secara sah.</p>
-                </div>
+                <p>
+                  Website ini menggunakan cookie dan teknologi pelacak lainnya untuk meningkatkan pengalaman penelusuran agar lebih sesuai dengan kebutuhan Anda. Dengan mengklik
+                  “Terima”, Anda menyetujui pemrosesan data pribadi Anda sesuai dengan <Link href="/kebijakan-privasi">Kebijakan Privasi dan Kebijakan Cookie</Link>
+                </p>
+                {!collapsed && (
+                  <>
+                    <p>Kami tidak membagikan informasi pribadi Anda dengan pihak ketiga, kecuali jika dibutuhkan untuk kebutuhan profesional dan bisnis kami secara sah.</p>
 
-                <div>
-                  <h3 className="text-2xl md:text-3xl font-semibold">Poin Penting</h3>
-                  <ul>
-                    <li className="list-disc">Kami mengumpulkan data pribadi seperti alamat IP, perilaku penelusuran, dan informasi lain yang dibutuhkan untuk fungsionalitas dan peningkatan situs web.</li>
-                    <li className="list-disc">Kami mungkin membagikan data Anda kepada pihak ketiga yang terpercaya.</li>
-                    <li className="list-disc">Anda memiliki hak untuk mengakses, memperbaiki dan menghapus data Anda.</li>
-                  </ul>
-                  <p>Detail informasi tambahan terkait data yang kami kumpulkan dan hak Anda, dapat mengunjungi <Link href='/kebijakan-privasi'>Kebijakan Privasi</Link></p>
-
-                  {!collapsed && <div>
-                    <h3 className="text-2xl md:text-3xl font-semibold">Options dengan kelola preferensi</h3>
-                    <p>Kelola Preferensi:</p>
                     <div>
-                      <label className="flex gap-2 items-center">
-                        <input type="checkbox" />
-                        Cookie Esensial (dibutuhkan untuk fungsionalitas situs web)
-                      </label>
-                      <label className="flex gap-2 items-center">
-                        <input type="checkbox" />
-                        Cookie Analytics (membantu kami untuk meningkatkan performa situs web)
-                      </label>
-                      <label className="flex gap-2 items-center">
-                        <input type="checkbox" />
-                        Cookie Pemasaran (iklan dipersonalisasi)
-                      </label>
+                      <h3 className="text-2xl md:text-3xl font-semibold">Poin Penting</h3>
+                      <ul>
+                        <li className="list-disc">
+                          Kami mengumpulkan data pribadi seperti alamat IP, perilaku penelusuran, dan informasi lain yang dibutuhkan untuk fungsionalitas dan peningkatan situs web.
+                        </li>
+                        <li className="list-disc">Kami mungkin membagikan data Anda kepada pihak ketiga yang terpercaya.</li>
+                        <li className="list-disc">Anda memiliki hak untuk mengakses, memperbaiki dan menghapus data Anda.</li>
+                      </ul>
+                      <p>
+                        Detail informasi tambahan terkait data yang kami kumpulkan dan hak Anda, dapat mengunjungi <Link href="/kebijakan-privasi">Kebijakan Privasi</Link>
+                      </p>
+
+                      <div>
+                        <h3 className="text-2xl md:text-3xl font-semibold">Options dengan kelola preferensi</h3>
+                        <p>Kelola Preferensi:</p>
+                        <div>
+                          <label className="flex gap-2 items-center">
+                            <input type="checkbox" />
+                            Cookie Esensial (dibutuhkan untuk fungsionalitas situs web)
+                          </label>
+                          <label className="flex gap-2 items-center">
+                            <input type="checkbox" />
+                            Cookie Analytics (membantu kami untuk meningkatkan performa situs web)
+                          </label>
+                          <label className="flex gap-2 items-center">
+                            <input type="checkbox" />
+                            Cookie Pemasaran (iklan dipersonalisasi)
+                          </label>
+                        </div>
+
+                        <p>Dengan terus menggunakan situs web ini tanpa mengubah pengaturan, Anda menyetujui atas penggunaan cookie dan teknologi penelusuran lainnya.</p>
+                      </div>
                     </div>
-
-                    <p>Dengan terus menggunakan situs web ini tanpa mengubah pengaturan, Anda menyetujui atas penggunaan cookie dan teknologi penelusuran lainnya.</p>
-
-                  </div>}
-
-                </div>
+                  </>
+                )}
 
                 <div className="w-full flex flex-col md:flex-row items-center">
                   {/* <button onClick={cancelConfirm} className="hover:underline transition-all duration-300 ease-in py-4 w-full rounded-lg text-2xl"> */}
-                  {collapsed && <button onClick={() => setCollapsed(false)} className="bg-[#154881] hover:bg-[#154881e6] transition-all duration-100 ease-in text-white py-4 w-full rounded-lg text-2xl">
-                    Mengelola Preferensi
-                  </button>}
-                  <button onClick={handleConfirm} className="hover:underline transition-all duration-300 ease-in py-4 w-full rounded-lg text-2xl">
+                  {collapsed && (
+                    <button onClick={() => setCollapsed(false)} className="hover:underline transition-all duration-300 ease-in py-4 w-full rounded-lg text-2xl ">
+                      Mengelola Preferensi
+                    </button>
+                  )}
+                  <button onClick={handleConfirm} className="bg-[#154881] hover:bg-[#154881e6] transition-all duration-100 ease-in text-white py-4 w-full rounded-lg text-2xl">
                     Terima Semua
                   </button>
                 </div>
